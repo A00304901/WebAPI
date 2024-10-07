5688e7ab0b7050a11357493d6c1ecdb0c63e5231
A00304901 <163214232+A00304901@users.noreply.github.com>
2024-10-07 22:18:40 +0000

    Secondly,I have added ICalculatorService interface in the Interfaces folder

---
+++ Interfaces/ICalculatorService.cs
@@ -0,0 +1,10 @@
+namespace ToDoListAPI.Interfaces
+{
+    public interface ICalculatorService
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}
