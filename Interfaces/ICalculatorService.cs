f81ecad11978cd586c0f5656b97cf75f78ea4bdb
A00304901 <163214232+A00304901@users.noreply.github.com>
2024-10-07 22:30:12 +0000

    now again in this step, I have Updated CalculatorService to inherit ICalculatorService with double type

--- Interfaces/ICalculatorService.cs
+++ Interfaces/ICalculatorService.cs
@@ -1,10 +1,29 @@
-namespace ToDoListAPI.Interfaces
+using ToDoListAPI.Interfaces;
+
+namespace ToDoListAPI.Services
 {
-    public interface ICalculatorService<T>
+    public class CalculatorService : ICalculatorService<double>
     {
-        T Add(T a, T b);
-        T Subtract(T a, T b);
-        T Multiply(T a, T b);
-        T Divide(T a, T b);
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
     }
 }
